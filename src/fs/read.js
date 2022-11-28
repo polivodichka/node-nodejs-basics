6306d2af6f042109abaa7bf5a26a8f0fe8248db4
Anastasia Polivoda <stacy99anastasia@gmail.com>
2022-11-28 23:13:43 +0300

    fix: fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -5,9 +5,9 @@ import path from 'path';
 const toRead = path.resolve(path.dirname(''), 'src', 'fs', 'files', 'fileToRead.txt');
 
 export const read = async () => {
-    fs.readFile(toRead, 'utf8', function(error,data){
-        if(error) throw new Error ('FS operation failed');; 
-        console.log(data); 
+    fs.readFile(toRead, 'utf8', function (error, data) {
+        if (error) try { throw new Error('FS operation failed') } catch (error) { console.log(error.message) }
+        else console.log(data);
     });
 };
 
