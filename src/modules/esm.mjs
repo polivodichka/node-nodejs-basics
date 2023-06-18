4402e47fea9509f656515d742943d024557f5cad
Anastasia Polivoda <polivoda.anastasiia@gmail.com>
2023-06-18 16:17:39 +0300

    fix: esm.mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -23,12 +23,21 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${path.join(fileURLToPath(import.meta.url))}`);
 console.log(`Path to current directory is ${path.join(fileURLToPath(import.meta.url), '..')}`);
 
-const createMyServer = createServerHttp((_, res) => {
+const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject.default);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
 });
 
 export default {
     unknownObject,
-    createMyServer,
+    myServer,
 };
 
